2ae5de325121b596bf263df93bb47216c5412d0d
OleksandraDana <110694602+OleksandraDana@users.noreply.github.com>
2023-02-22 20:13:30 +0000

    started working on updated time/date

--- src/app.js
+++ src/app.js
@@ -1,15 +1,24 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let day = date.getDay();
+  return `${day}${hours}:${minutes}`;
+}
+
 function showTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "197a5ad85f9efe7ac594ce5195482502";
